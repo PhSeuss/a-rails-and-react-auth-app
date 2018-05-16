599b5d77ae3668b4eed21dee7445fa2e9b55de10
thuong <truonghoaithuong2000@gmail.com>
2018-05-16 22:35:08 +0700

    create monster table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514151715) do
+ActiveRecord::Schema.define(version: 20180516153407) do
+
+  create_table "monsters", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "username"
