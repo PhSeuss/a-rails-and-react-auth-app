68cead7e83cffe951f3c3fd63af4c162d715237c
Thuong <truonghoaithuong2000@gmail.com>
2018-05-20 20:58:56 +0700

    dashboard

---
+++ react-client/src/components/DashBoard.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import Auth from '../modules/Auth';
+import AddMonsterForm from './AddMonsterForm';
+class DashBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myMonster: null,
+      monsterLoaded: false
+    };
+    this.addMonster = this.addMonster.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUserMonsters();
+  }
+  getUserMonsters() {
+    fetch('/profile', {
+      method: 'GET',
+      headers: {
+        token: Auth.getToken(),
+        Authorization: `Token ${Auth.getToken()}`
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({
+          myMonster: res.user.monsters,
+          monsterLoaded: true
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  addMonster(e, data) {
+    fetch('/monsters', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        token: Auth.getToken(),
+        Authorization: `Token ${Auth.getToken()}`
+      },
+      body: JSON.stringify({
+        monster: data
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.getUserMonsters();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  renderMonster() {
+    return this.state.myMonster ? (
+      this.state.myMonster.map(monster => (
+        <h1 key={monster.id}>{monster.name}</h1>
+      ))
+    ) : (
+      <h3>Create a monster</h3>
+    );
+  }
+  render() {
+    return (
+      <div className="dash">
+        <AddMonsterForm addMonster={this.addMonster} />
+        {this.state.monsterLoaded ? this.renderMonster() : <h2>Loading...</h2>}
+      </div>
+    );
+  }
+}
+
+export default DashBoard;
