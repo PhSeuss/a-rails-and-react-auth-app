95f56d474ace57d5944c39d5711c03b227831414
Thuong <truonghoaithuong2000@gmail.com>
2018-05-29 01:38:51 +0700

    better solution for api fallback on rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
     include ActionController::HttpAuthentication::Token::ControllerMethods
+    include ActionView::Rendering
     def fallback_index_html
       render :file => 'public/index.html'
     end
